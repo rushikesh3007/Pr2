09b7a486f80ae1cb8ccf8f6b31ddecf00f2494b8
rushikesh3007 <ghogarerushi52@gmail.com>
2025-03-25 23:59:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-a=int (input("Enter Your First No: "))
-b=int(input('Enter Your Second No: '))
+a=int(input("Enter Your First No: "))
+b=int(input("Enter Your Second No: "))
 c=a+b
 print("Addtion Of %i and %i is %i" %(a,b,c))
